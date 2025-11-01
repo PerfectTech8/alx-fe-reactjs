b803ef1c0ac4c4bae505997bffb620a8800ea1d3
Nnanta Charles <nnantaprince2@gmail.com>
2025-11-01 14:08:40 +0100

    created a user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -25,6 +26,10 @@ function App() {
       </div>
       <div>
       <Footer />
+    </div>
+
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
 
 
