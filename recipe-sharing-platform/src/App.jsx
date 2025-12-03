3e2e1cb38fc01d6a2372d8a5be229a072a7aa96f
Nnanta Charles <nnantaprince2@gmail.com>
2025-12-03 18:35:15 +0100

    created a homepage

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './components/HomePage'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1 className="text-4xl font-bold text-blue-600">
-      Tailwind is working!
-     </h1>
+      <HomePage />
     </div>
 
     
